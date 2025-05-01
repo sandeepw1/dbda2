3390f18cc025dd4b410c5220f106c490b981e8a8
sandeepw1 <demo.labs08@gmail.com>
2025-05-01 20:19:19 +0530

    Update index.py

--- index.py
+++ index.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def hello():
-    return "Hello, World!"
+    return "This app is hosted successfully on Openshift"
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=9000, debug=True)
