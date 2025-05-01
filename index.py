c6a7a06ff37e1901ba50d95ac5d4e716ec55d26f
sandeepw1 <demo.labs08@gmail.com>
2025-05-01 21:45:16 +0530

    Update index.py

--- index.py
+++ index.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def hello():
-    return "This app build is automatic...Webhook with secret added...finally???"
+    return "This app build is automatic...working.finally???"
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=9000, debug=True)
